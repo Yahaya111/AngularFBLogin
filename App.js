f51b8dc66de758ffc0aea59386b66ce801d9f993
Yahaya111 <yahmak44@yahoo.com>
2016-07-25 15:15:40 +0100

    js files committed

--- App.js
+++ App.js
@@ -2,13 +2,32 @@
 
   'use strict';
 
-  angular
-    .module('authApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router'])
+  angular.module('authApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngMaterial', 'ui.router'])
     .config(function($provide, authProvider, $urlRouterProvider, $stateProvider, $httpProvider, jwtInterceptorProvider) {
 
       authProvider.init({
         domain: 'yahaya111.auth0.com',
         clientID: 'vpxavBzZH04rN7IMNw8WIRJpRCncAj36',
       });
+      
+      $urlRouterProvider.otherwise("/home");
+
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: 'Components/home/Home.html'
+        })
+        .state('profile', {
+          url: '/profile',
+          templateUrl: 'Components/profile/Profile.html',
+          controller: 'profileController as user'
+        });
+
+      jwtInterceptorProvider.tokenGetter = function(store) {
+        return store.get('token');
+      }
+
+      $httpProvider.interceptors.push('jwtInterceptor');
+      
     });
 })();
